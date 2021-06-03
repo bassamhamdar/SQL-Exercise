ab2638dd811a73d93cbc5d6c86650206b02abae6
bassamhamdar <hamdar.m.bassam@gmail.com>
2021-06-03 22:00:15 +0300

    Basic Queries

---
+++ answers.sh
@@ -0,0 +1,7 @@
+Problem 1: SELECT name FROM students;
+Problem 2: SELECT * FROM students WHERE Age > 30;
+problem 3: SELECT name FROM students WHERE Age = 30 AND Gender = 'F';
+problem 4: SELECT Points FROM students WHERE name = 'Alex';
+problem 5: INSERT INTO students(name, Age, Gender, Points) VALUES ('Bassam', 27, 'M', 4000);
+problem 6: UPDATE students SET Points = Points + 10 WHERE name = 'Basma';
+problem 7: UPDATE students SET Points = Points - 10 WHERE name = 'Alex';
